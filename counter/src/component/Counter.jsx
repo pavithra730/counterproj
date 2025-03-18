b61fa5b3e3def1dded2160754aac964000cba0b5
pavithra730 <pavithraravi773@gmail.com>
2025-03-18 21:26:12 +0530

    add sound to button

--- counter/src/component/Counter.jsx
+++ counter/src/component/Counter.jsx
@@ -1,13 +1,56 @@
 import React, { useState } from 'react'
 
 export const Counter = () => {
-    const [count,setcount]=useState(0)
+    const [count,setcount]=useState(0);
+    const mincount=0;
+    const maxcount=10;
+
+    const increase=()=>{
+        if(count<maxcount){
+            setcount(count+1);
+            playsound();
+        }
+        else{
+            alert("out of limit")
+        }
+        
+    }
+    
+    const decrease=()=>{
+        if(count<mincount){
+            alert("going on negative values");
+            
+        }
+        
+        else{
+            setcount(count-1);
+            playsound();
+        }
+        
+    }
+
+    const colorcount=(count)=>{
+        if(count>=5){
+            return "red"
+        }
+        else{
+            return "aqua"
+        }
+    }
+
+    const playsound=()=>{
+        const audio=new Audio("not.mp3");
+        audio.play();
+    }
+
+
+
   return (
     <div>
        <div className="counter">
-        <h2 className='h'>Counter: {count}</h2>
-        <button onClick={()=>{setcount(count+1)}}>Increase</button>
-        <button onClick={()=>{setcount(count-1)}}>Decrease</button>
+        <h2 className='h' style={{color:colorcount(count)}}>Counter: {count}</h2>
+        <button onClick={increase} disabled={count===maxcount}>Increase</button>
+        <button onClick={decrease} disabled={count===mincount}>Decrease</button>
         <button onClick={()=>{setcount(0)}}>Reset</button>
        </div>
 
